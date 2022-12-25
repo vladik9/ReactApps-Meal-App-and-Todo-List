485bc0039f1b5eae259903478093a08c6595655e
Vlad <v.cornici@gmail.com>
2022-12-25 15:54:39 +0200

    added functionality for checking if an list have no duplicates

--- LearnReactApps/meals-add-app/src/Components/context/CartContextProvider.js
+++ LearnReactApps/meals-add-app/src/Components/context/CartContextProvider.js
@@ -29,11 +29,32 @@ const mealsListArray = [
 const defaultCardState = { items: [], totalAmount: 0 };
 const cartReeducer = (state, action) => {
   if (action.type === "ADD_ITEM") {
-    const updatedArray = state.items.concat(action.item);
-    const totalAmount = (state.totalAmount +=
-      action.item.price * action.item.amount);
-    return { items: updatedArray, totalAmount };
+    console.log("state ", state, "action ", action);
+
+    if (state.items.length === 0) {
+      const updatedArray = state.items.concat(action.item);
+      const totalAmount = (state.totalAmount +=
+        action.item.price * action.item.amount);
+      return { items: updatedArray, totalAmount };
+    } else {
+      // const index = state.items.map((item) => item.id.indexOf(action.item.id));
+      const index = state.items.includes(action.item);
+
+      const newArray = state.items.concat(action.item);
+      console.log(newArray);
+      console.log("index on=", index);
+      //need to work here as this should check if an item is already in the list and add it.
+      if (!index) {
+        const updatedArray = state.items.concat(action.item);
+        const totalAmount = (state.totalAmount +=
+          action.item.price * action.item.amount);
+        return { items: updatedArray, totalAmount };
+      } else {
+        return { items: state.item, totalAmount: state.totalAmount };
+      }
+    }
   }
+
   if (action.type === "REMOVE_ITEM") {
     const updatedArray = state.items.filter(
       (item) => item.id !== action.itemId
